7406d12b6a1971fbadcd8a736274bb5aacdf090f
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-10 17:22:40 +0300

    Completed Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# my_function_return_value = my_function(1)
+# print(my_function_return_value)
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    print(sum)
+    return sum
+
+add(1,2)
 
 def halve(number):
-    pass
+    if isinstance(number, (int,float)):
+        result = number / 2
+        print(result)
+        return result
+    else:
+        return None
+
+halve(12)
